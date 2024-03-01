a9dec0f905d442e6e1f7e4379c2df6735a667aa2
YoanaNakova <yoana.nakova@egt-digital.com>
2024-03-01 20:12:35 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { HomePage } from "../src/pages/Home";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
